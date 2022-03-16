bac639eb235760b89538fa402035e41db1de27a8
mr128254 <moritz.reh@gmail.com>
2022-03-16 13:51:58 +0100

    made a fixed size stepper class.

--- vmc_fluids/stepper.py
+++ vmc_fluids/stepper.py
@@ -3,45 +3,6 @@ import jax.numpy as jnp
 import numpy as np
 
 
-class Euler:
-    ''' This class implements Euler integration
-    '''
-
-    def __init__(self, timeStep=1e-3):
-
-        self.dt = timeStep
-
-    def step(self, t, f, yInitial, **rhsArgs):
-        """ This function performs an integration time step.
-
-        For a first order ordinary differential equation (ODE) of the form
-
-        :math:`\\frac{dy}{dt} = f(y, t, p)`
-
-        where :math:`t` denotes the time and :math:`p` denotes further external parameters
-        this function computes the Euler integration step
-
-        :math:`y_{n+1} = y_n+\\Delta t f(y_n,t_n,p)`
-
-        Arguments:
-            * ``t``: Initial time.
-            * ``f``: Right hand side of the ODE. This callable will be called as ``f(y, t, **rhsArgs, intStep=k)``, \
-                    where k is an integer indicating the step number of the underlying Runge-Kutta integration scheme.
-            * ``y``: Initial value of :math:`y`.
-            * ``**rhsArgs``: Further static arguments :math:`p` that will be passed to the right hand side function \
-                    ``f(y, t, **rhsArgs, intStep=k)``.
-
-        Returns:
-            New value of :math:`y` and time step used :math:`\\Delta t`.
-        """
-
-        dy, info = f(yInitial, t, **rhsArgs, intStep=0)
-
-        return yInitial + self.dt * dy, self.dt, info
-
-# end class Euler
-
-
 class AdaptiveHeun:
     """ This class implements an adaptive second order consistent integration scheme.
 
@@ -127,3 +88,53 @@ class AdaptiveHeun:
         self.dt = dt
 
         return yInitial + dy1, realDt, info
+
+
+class FixedStepper:
+    """ This class implements an two integration schemes with fixed step sizes.
+
+    Initializer arguments:
+        * ``timeStep``: Initial time step (will be adapted automatically)
+        * ``tol``: Tolerance for integration errors.
+        * ``maxStep``: Maximal allowed time step.
+    """
+
+    def __init__(self, timeStep=1e-3, mode='Heun'):
+        self.dt = timeStep
+        self.mode = mode
+
+    def step(self, t, f, y, normFunction=jnp.linalg.norm, **rhsArgs):
+        """ This function performs an integration time step.
+
+        For a first order ordinary differential equation (ODE) of the form
+
+        :math:`\\frac{dy}{dt} = f(y, t, p)`
+
+        Arguments:
+            * ``t``: Initial time.
+            * ``f``: Right hand side of the ODE. This callable will be called as ``f(y, t, **rhsArgs, intStep=k)``, \
+                    where k is an integer indicating the step number of the underlying Runge-Kutta integration scheme.
+            * ``y``: Initial value of :math:`y`.
+            * ``normFunction``: Norm function to be used to quantify the magnitude of errors.
+            * ``**rhsArgs``: Further static arguments :math:`p` that will be passed to the right hand side function \
+                    ``f(y, t, **rhsArgs, intStep=k)``.
+
+        Returns:
+            New value of :math:`y` and time step used :math:`\\Delta t`.
+        """
+
+        yInitial = y.copy()
+
+        if self.mode == 'Heun':
+            y = yInitial.copy()
+            k0, _ = f(y, t, **rhsArgs, intStep=0)
+            y += self.dt * k0
+            k1, info = f(y, t + self.dt, **rhsArgs, intStep=1)
+            dy = 0.5 * self.dt * (k0 + k1)
+            return yInitial + dy, self.dt, info
+
+        if self.mode == 'Euler':
+            y = yInitial.copy()
+            k0, info = f(y, t, **rhsArgs, intStep=0)
+            dy = self.dt * k0
+            return yInitial + dy, self.dt, info
